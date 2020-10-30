e81ed527e2c27dd2b848966d559027f636da37ab
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-10-31 06:05:31 +0700

    :rocket: Update configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,6 @@
 export default {
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-
-  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Nuxt Plyr',
     meta: [
@@ -17,33 +12,16 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
       'plyr/dist/plyr.css',
-      '@/assets/css/overwrite.css',
-      '@/assets/css/global.css'
+      '@/src/assets/css/overwrite.css',
+      '@/src/assets/css/global.css'
   ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-      '@/plugins/nuxt-plyr'
+      '@/src/plugins/nuxt-plyr'
   ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
 }
