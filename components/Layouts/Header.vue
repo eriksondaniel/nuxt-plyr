6b0c48ba9f839a0d779b0f540cd79de94b5dec6b
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-10-31 06:47:38 +0700

    :sparkles: Setup wave background

--- components/Layouts/Header.vue
+++ components/Layouts/Header.vue
@@ -1,14 +1,14 @@
 <template>
     <header class="flex items-center justify-between">
         <a href="">
-            <h2 class="text-6xl font-bold uppercase flex items-center">
+            <h2 class="text-5xl font-bold uppercase flex items-center">
                 <!-- <Logo class="mr-3"/> -->
                 <span class="heading-stroke" data-title="Nuxt Plyr">
                     Nuxt Plyr
                 </span>
             </h2>
         </a>
-        <a href="https://github.com/dyprast" target="blank">
+        <a href="https://github.com/dyprast/nuxt-plyr" target="blank">
             <IconGithub/>
         </a>
     </header>
